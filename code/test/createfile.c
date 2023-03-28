6ec53674066a144ae63058190fdf1718739e6f83
lucky634634 <khoa21102003@gmail.com>
2023-03-28 13:25:58 +0700

    Update gitignore

---
+++ code/test/createfile.c
@@ -0,0 +1,24 @@
+#include "syscall.h"
+#include "copyright.h"
+
+int main()
+{
+    char *filename = "text.txt";
+
+    // Create a file;
+    if (CreateFile(filename) == -1)
+    {
+        // Error
+        print("\nCreate file ");
+        print(filename);
+        print(" fail");
+    }
+    else
+    {
+        // Success
+        print("\nCreate file ");
+        print(filename);
+        print(" successfully\n");
+    }
+    Halt();
+}
